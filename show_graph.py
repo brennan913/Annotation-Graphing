6ad0c7ab2678d9de39f15259c2599ccea33f4b67
BXM <brennan913@gmail.com>
2019-03-05 01:24:05 -0500

    cleaned up show_graph.py

--- show_graph.py
+++ show_graph.py
@@ -1,19 +1,14 @@
 import sys
-#import matplotlib
 import matplotlib.pyplot as plt
-#import plotly.plotly as ply
-#import plotly.graph_objs as go
 import networkx as nx
 import networkx.drawing
-
 
 from ann_to_graph import *
 
 def main(argc, argv):
-    test4(argc, argv)
-    #test3()
+    show_graph(argc, argv)
 
-def test4(argc, argv):
+def show_graph(argc, argv):
     if argc != 2:
         sys.stderr.write('usage: {} <filename>\n'.format(argv[0]))
         return 1
@@ -25,42 +20,16 @@ def test4(argc, argv):
     plt.ioff()
     G = nx.DiGraph(dod)
     pos = nx.spring_layout(G)
-    nx.draw(G, pos=pos, with_labels=True, font_weight='bold', font_size=7, node_size=3000, node_color='beige', labels={n:n for n in G.nodes()})
+    nx.draw(G, pos=pos, with_labels=True, font_weight='bold', font_size=5, node_size=2500, node_color='beige', labels={n:n for n in G.nodes()})
+    
     labels = {}
     for edge in edges:
         labels[(edge.start, edge.end)]=edge.relation.name
 
-    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=labels, font_color='xkcd:blue gray')
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=labels, font_size=10, font_color='xkcd:blue gray')
     plt.axis('on')
     plt.show()
-
-def test2():
-    plt.ioff()
-    G = nx.petersen_graph()
-    nx.draw(G, with_labels=True, font_weight='bold')
-    #print(plt.subplot(122))
-    nx.draw_shell(G, nlist=[range(5,10), range(5)], with_labels=True, font_weight='bold')
-    plt.show()
-
-def test3():
-    plt.ioff()
-    dod= {'start': {'almost':{'weight':1}}, 'almost': {'end':{'weight':1000}}} # single edge (0,1)
-    #G=nx.from_dict_of_dicts(dod)
-    G = nx.DiGraph(dod)
-    nx.draw(G, with_labels=True, font_weight='bold', node_color='beige')
-    #nx.draw_networkx_edge_labels()
-    plt.show()
-
-def test1(argc, argv):
-    if argc != 2:
-        sys.stderr.write('usage: {} <filename>\n'.format(argv[0]))
-        return 1
     
-    edges = generate_edges(argv[1])
-    print()
-    print(edges)
-    print()
-
 
 if __name__=="__main__":
     main(len(sys.argv), sys.argv)
